a00bc23eb428d24143615f2a3bee695125d00d07
Sainath Reddy <dukedustine123@gmail.com>
2021-11-20 16:12:34 +0530

    commits

---
+++ app/src/main/java/com/masai/sainath/salonbookingapp/OnItemClickListener.kt
@@ -0,0 +1,10 @@
+package com.masai.sainath.salonbookingapp
+
+
+interface OnItemClickListener {
+
+    fun onItemClicked(mallItem: AdminModel)
+
+    fun onDirectionsClicked()
+
+}
---
+++ app/src/main/java/com/masai/sainath/salonbookingapp/UserAdapter1.kt
@@ -0,0 +1,23 @@
+package com.masai.sainath.salonbookingapp
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+
+class UserAdapter1(private val list: List<AdminModel>,private val itemClickListener: OnItemClickListener) :
+    RecyclerView.Adapter<User1viewholder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): User1viewholder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_users1, parent, false)
+        return User1viewholder(view,itemClickListener)
+    }
+
+    override fun onBindViewHolder(holder: User1viewholder, position: Int) {
+        val mallItem = list[position]
+        holder.setData(mallItem)
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+}
