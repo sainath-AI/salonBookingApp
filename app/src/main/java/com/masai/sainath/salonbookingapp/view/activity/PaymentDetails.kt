56f67e694418bb677d01409245aa6f291eb4e367
Sainath Reddy <dukedustine123@gmail.com>
2021-11-21 15:00:06 +0530

    sai commits

--- app/src/main/java/com/masai/sainath/salonbookingapp/view/activity/PaymentDetails.kt
+++ app/src/main/java/com/masai/sainath/salonbookingapp/view/activity/PaymentDetails.kt
@@ -1,7 +1,10 @@
 package com.masai.sainath.salonbookingapp.view.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
 import com.bumptech.glide.Glide
 import com.masai.sainath.salonbookingapp.R
 import kotlinx.android.synthetic.main.activity_payment_details.*
@@ -15,7 +18,14 @@ class PaymentDetails : AppCompatActivity() {
 
         val imgurl = intent.getStringExtra("imgurl")
         btnPayment.setOnClickListener {
-
+            paymentCard.visibility= View.INVISIBLE
+            cartAnimation.visibility=View.VISIBLE
+            cartAnimation.playAnimation()
+            Handler().postDelayed({
+                intent= Intent(this,UserActivtiy::class.java)
+                startActivity(intent)
+                finish()
+            },3000)
         }
 
 
